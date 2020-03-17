3ef2b79cfa4c1fc554e0b797b1c3653d94dc80cc
Sugarplum25 <sugarplamka@gmail.com>
2020-03-17 12:20:35 +0400

    add img, basic html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: ['./src/scss/main.scss'],
@@ -31,6 +32,17 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
       },
     ]
   },
